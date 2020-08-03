77a4f4a5e4c3cad24c22439cd3869da8f6d9c51c
ErPratham <44560611+pratham3164@users.noreply.github.com>
2020-08-04 01:06:19 +0530

    Update restconfig.js

--- project/config/restconfig.js
+++ project/config/restconfig.js
@@ -1,6 +1,6 @@
 export const article_url = 'https://newsapi.org/v2/top-headlines';
 export var lang = 'en';
-export const _api_key = '7031e66418a54a04b240e65e9537585e';
+export const _api_key = '_getYourApiKey from newsapi.org';
 
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
